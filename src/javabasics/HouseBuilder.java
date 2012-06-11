c4702c40a1cfea322e670c4617ac9738c5341a83
aglassman <andrew.glassman@gmail.com>
2012-06-11 12:00:02 -0500

    Updated comment.

--- src/javabasics/HouseBuilder.java
+++ src/javabasics/HouseBuilder.java
@@ -46,6 +46,11 @@ public class HouseBuilder {
 	
 	private void polymorphismExample()
 	{
+		/*
+		 * PolymorphismHouse inherits functionality from House by using the extends
+		 * keyword.  It then uses Polymorphism to change the behavior of the countDoors
+		 * method.
+		 */
 		PolymorphismHouse polyHouse = new PolymorphismHouse(5,5);
 		System.out.println("My polyHouse has " + polyHouse.countDoors() + " doors.(method override, excludes back doors.)");
 		System.out.println("My polyHouse has " + polyHouse.countAllDoors() + " doors.(calling overridden method from House, all doors counted");
